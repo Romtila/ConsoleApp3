d9c9763332de3787da0baca7d9ce7abe84f87535
Romtila <romtila@yandex.ru>
2020-12-06 14:33:54 +1000

    Start new task

---
+++ ConsoleApp3/Models/CommentService.cs
@@ -0,0 +1,16 @@
+﻿namespace ConsoleApp3
+{
+    public class CommentService
+    {
+        private readonly UserService _userService;
+
+        private readonly VideoService _videoService;
+
+        public CommentService(VideoService videoService, UserService userService)
+        {
+            _videoService = videoService;
+            _userService = userService;
+        }
+    }
+}
+
--- ConsoleApp3/Models/UserView.cs
+++ ConsoleApp3/Models/UserView.cs
@@ -23,9 +23,4 @@ namespace ConsoleApp3
         public DateTime BirthDay { get; set; }
     }
 }
-// создать класс userService, в который передаем модель UserView, а внутри добавить проверки, 
-// например чтобы длина логина была длиннее чегонить, пароль проверка, несколько полей добавить в документ User, типа мейл и др.
-// а потом UserrService создает мне User по UserView. 
-// и тоже самое VideoService аналогично с userService, в классе Video + должен быть Object ID "OwnerId", это Id того юзера, котоырй его создал  
-// Когад мы пытаемся создать видео, проверяем, что User (OwnerId в Video) реально существует такой User
 
